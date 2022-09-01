e6f0ed27ff6ed79aa091e13bcb8eab4e994439b0
Aishiela ayu permatasari <aishielaayu24@gmail.com>
2022-09-01 23:13:16 +0800

    frontend

---
+++ resources/views/auth/login2.blade.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.98.0">
+    <title>Signin Template · Bootstrap v5.2</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/sign-in/">
+
+
+
+
+
+{{-- <link href="{{asset('bootstrap/assets/dist/css/bootstrap.min.css')}}" rel="stylesheet"> --}}
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+{{-- <link rel="stylesheet" href="{{asset('bootstrap/sign-in/signin.css')}}"> --}}
+<link rel="stylesheet" href="{{asset('bootstrap/sign-in/registrasi.css')}}">
+
+
+    <!-- Custom styles for this template -->
+    <link href="{{asset('bootsrap/sign-in/signin.css')}}" rel="stylesheet">
+  </head>
+  <body class="text-center">
+
+<main class="form-signin w-101 m-auto">
+  <form>
+    <img class="mb-4" src="{{asset('bootstrap/assets/brand/bootstrap-logo.svg')}}" alt="" width="72" height="57">
+    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+    <div class="form-floating">
+      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+      <label for="floatingInput">Email address</label>
+    </div>
+    <div class="form-floating">
+      <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+      <label for="floatingPassword">Password</label>
+    </div>
+
+    <div class="checkbox mb-3">
+      <label>
+        <input type="checkbox" value="remember-me"> Remember me
+      </label>
+    </div>
+    <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+    <p class="mt-5 mb-3 text-muted">&copy; 2017–2022</p>
+  </form>
+</main>
+<script src="{{asset('bootstrap/assets/dist/js/bootstrap.bundle.min.js')}}"></script>
+<script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script><script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous"></script><script src="{{asset('bootstrap/dashboard/dashboard.js')}}"></script>
+
+
+  </body>
+</html>
